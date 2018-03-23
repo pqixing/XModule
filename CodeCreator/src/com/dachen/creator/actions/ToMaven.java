597b515cd6def051c1c526a7589dff95fe761897
pqixing <pqixing>
2018-03-24 00:48:01 +0800

    提交代码

--- CodeCreator/src/com/dachen/creator/actions/ToMaven.java
+++ CodeCreator/src/com/dachen/creator/actions/ToMaven.java
@@ -1,21 +1,14 @@
 package com.dachen.creator.actions;
 
-import com.dachen.creator.utils.GradleUtils;
-import com.intellij.codeInsight.navigation.BackgroundUpdaterTask;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.application.ModalityState;
-import com.intellij.openapi.externalSystem.task.TaskCallback;
-import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.InputValidator;
+import com.intellij.openapi.ui.DialogBuilder;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.util.PairFunction;
 
 import javax.swing.*;
-import java.util.Arrays;
 
 public class ToMaven extends AnAction {
 
@@ -35,7 +28,7 @@ public class ToMaven extends AnAction {
 //                System.out.println("onFailure-------------");
 //            }
 //        });
-        JTextField jTextField = new JTextField();
+//        JTextField jTextField = new JTextField();
 //        Messages.showMultilineInputDialog(project, "提示信息", "多行", "哈哈哈", null, new InputValidator() {
 //            @Override
 //            public boolean checkInput(String s) {
@@ -49,13 +42,9 @@ public class ToMaven extends AnAction {
 //                return false;
 //            }
 //        });
-
-        Messages.showCheckboxMessageDialog("多选", " ti ", new String[]{"1", "2", "3"}, "请选择", true, 2, 2, null, new PairFunction<Integer, JCheckBox, Integer>() {
-            @Override
-            public Integer fun(Integer integer, JCheckBox jCheckBox) {
-                System.out.println(integer+"-------"+jCheckBox.getText());
-                return 2;
-            }
+        Messages.showCheckboxMessageDialog("多选", " ti ", new String[]{"1", "2", "3"}, "请选择", true, 2, 2, null, (integer, jCheckBox) -> {
+            System.out.println(integer+"-------"+jCheckBox.getText());
+            return -1;
         });
 
 
