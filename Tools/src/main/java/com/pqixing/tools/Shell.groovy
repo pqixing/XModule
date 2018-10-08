728103115c610ff95f820502c190d97cae260c60
pqixing <pqixing@163.com>
2018-10-08 08:53:50 +0800

    for 3。0

--- Tools/src/main/java/com/pqixing/tools/Shell.groovy
+++ Tools/src/main/java/com/pqixing/tools/Shell.groovy
@@ -2,4 +2,10 @@ package com.pqixing.tools
 
 class Shell{
 
+
+    public static void run(String cmd){
+        def process = Runtime.getRuntime().exec(cmd)
+        process.in.readLines()
+        process.out
+    }
 }
