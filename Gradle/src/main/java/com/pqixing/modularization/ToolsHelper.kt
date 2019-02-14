8e50c17eaa9c5d01fe03caa30de9e04695db14a4
pqixing <pqixing@163.com>
2019-02-14 15:08:47 +0800

    tools打包成jar

---
+++ Gradle/src/main/java/com/pqixing/modularization/ToolsHelper.kt
@@ -0,0 +1,4 @@
+package com.pqixing.modularization
+
+class ToolsHelper {
+}
--- Tools/src/main/java/com/pqixing/ProjectInfoFiles.kt
+++ Tools/src/main/java/com/pqixing/ProjectInfoFiles.kt
@@ -1,17 +1,17 @@
 package com.pqixing
 object ProjectInfoFiles {
     val files = listOf(
-"ProjectInfo/android/DevActivity.java",
-"ProjectInfo/android/AndroidManifest.xml",
-"ProjectInfo/android/DevApplication.java",
-"ProjectInfo/gradles/com.module.android.gradle",
-"ProjectInfo/gradles/com.module.application.gradle",
+"ProjectInfo/versions/version.properties",
+"ProjectInfo/project.xml",
+"ProjectInfo/gradle.properties",
 "ProjectInfo/gradles/com.module.maven.gradle",
 "ProjectInfo/gradles/com.module.dev.gradle",
+"ProjectInfo/gradles/com.module.manager.gradle",
+"ProjectInfo/gradles/com.module.application.gradle",
 "ProjectInfo/gradles/com.module.ktolin.gradle",
 "ProjectInfo/gradles/com.module.library.gradle",
-"ProjectInfo/gradles/com.module.manager.gradle",
-"ProjectInfo/project.xml",
-"ProjectInfo/build.gradle",
-"ProjectInfo/gradle.properties",
-"ProjectInfo/versions/version.properties" ) }
+"ProjectInfo/gradles/com.module.android.gradle",
+"ProjectInfo/android/DevApplication.java",
+"ProjectInfo/android/DevActivity.java",
+"ProjectInfo/android/AndroidManifest.xml",
+"ProjectInfo/build.gradle" ) }
