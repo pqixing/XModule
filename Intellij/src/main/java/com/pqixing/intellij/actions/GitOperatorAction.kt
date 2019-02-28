dffd7687ea9624c552b3b5707df1d669ffc44afc
pqixing <pqixing@163.com>
2019-02-28 22:03:03 +0800

    增加ToMaven任务调试完成

--- Intellij/src/main/java/com/pqixing/intellij/actions/GitOperatorAction.kt
+++ Intellij/src/main/java/com/pqixing/intellij/actions/GitOperatorAction.kt
@@ -3,11 +3,10 @@ package com.pqixing.intellij.actions
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 
-class GitOperatorAction:AnAction() {
-    companion object {
-        val actionListener = mutableSetOf<Runnable>()
-    }
+class GitOperatorAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project ?: return
+
 
     }
 }
