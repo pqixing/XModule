899535919ce4ad8e01a0c0e629b45522266740aa
pqixing <pqixing@163.com>
2019-05-11 22:03:38 +0800

    新增提示

--- Q-gradle/src/main/java/com/pqixing/modularization/android/AndroidPlugin.kt
+++ Q-gradle/src/main/java/com/pqixing/modularization/android/AndroidPlugin.kt
@@ -1,6 +1,7 @@
 package com.pqixing.modularization.android
 
 
+import com.pqixing.Tools
 import com.pqixing.model.SubModule
 import com.pqixing.model.SubModuleType
 import com.pqixing.modularization.FileNames
@@ -19,6 +20,7 @@ import com.pqixing.modularization.maven.ToMavenCheckTask
 import com.pqixing.modularization.maven.ToMavenTask
 import com.pqixing.tools.FileUtils
 import com.pqixing.tools.TextUtils
+import groovy.lang.Closure
 import org.gradle.api.Project
 import org.gradle.api.Task
 import java.io.File
@@ -65,9 +67,26 @@ open class AndroidPlugin : BasePlugin() {
     override val ignoreFields: Set<String> = emptySet()
 
     override fun linkTask(): List<Class<out Task>> = mutableListOf(DpsAnalysisTask::class.java, PrepareDevTask::class.java, ToMavenCheckTask::class.java, ToMavenTask::class.java, BuildApkTask::class.java)
-
+    var doAfterList: MutableList<Runnable> = mutableListOf()
     lateinit var dpsManager: DpsManager
+    lateinit var processInnerDps: Runnable
     override fun apply(project: Project) {
+        val extHelper = JGroovyHelper.getImpl(IExtHelper::class.java)
+        //在工程处理后，处理组件依赖
+        extHelper.setExtMethod(project, "endConfig") {
+            Tools.println("--------------endConfig() is deprecated and do nothing,you can remove it")
+        }
+        extHelper.setExtMethod(project, "doAfterEvaluate") { if (it is Closure<*>) doAfterList.add(it) }
+        project.afterEvaluate {
+            processInnerDps.run()
+            doAfterList.forEach { c ->
+                if (c is Closure<*>) {
+                    c.delegate = project
+                    c.resolveStrategy = Closure.DELEGATE_ONLY
+                }
+                c.run()
+            }
+        }
         this.p = project
         project.extensions.extraProperties.set(project.name, this)
         initSubModule(project)
@@ -77,47 +96,47 @@ open class AndroidPlugin : BasePlugin() {
         //创建配置读取
         val moduleConfig = CompatDps(project, dpsExt)
         project.extensions.add(Keys.CONFIG_MODULE, moduleConfig)
-
-
-        val extHelper = JGroovyHelper.getImpl(IExtHelper::class.java)
         //在工程处理后，处理组件依赖
-        extHelper.setExtMethod(project, "endConfig") {
+        processInnerDps = Runnable {
+            Tools.println("----------------------processing innerDps----------------------")
             dpsManager = DpsManager(this@AndroidPlugin, dpsExt)
             val dependencies = dpsManager.resolveDps()
-            project.apply(mapOf("from" to FileUtils.writeText(File(cacheDir, FileNames.GRADLE_DEPENDENCIES), dependencies, true)))
-//            compatOldPlugin(dpsExt)//无需再兼容
+            val dpPath = FileUtils.writeText(File(cacheDir, FileNames.GRADLE_DEPENDENCIES), dependencies, true)
+            project.apply(mapOf("from" to dpPath))
+            Tools.println("processing result:$dpPath")
+            Tools.println("-----------------------------------------------------------------")
         }
     }
-
-    private fun compatOldPlugin(dpsExt: DpsExtends) {
 //
-        val javaCacheDir = File(cacheDir, "java")
-        val groupName = ManagerPlugin.getExtends().groupName
-        val configStr = StringBuilder("package auto.$groupName.${TextUtils.numOrLetter(project.name).toLowerCase()};\n")
-                .append("public class ${TextUtils.className(project.name)}Config { \n")
-        //Config文件输出
-        val DP_CONFIGS_NAMES = dpsExt.compiles.map { "auto.$groupName.${TextUtils.numOrLetter(it.moduleName).toLowerCase()}.${TextUtils.className(it.moduleName)}Config" }
-                .sortedBy { it }.toString()
-        configStr.append("public static final String  DP_CONFIGS_NAMES = \"${DP_CONFIGS_NAMES.replace("[", "").replace("]", "")}\";\n")
-        val CONFIG = "auto.$groupName.${TextUtils.numOrLetter(project.name).toLowerCase()}.${TextUtils.className(project.name)}"
-        configStr.append("public static final String  LAUNCH_CONFIG = \"${CONFIG}Launch\";\n")
-        val NAME = TextUtils.numOrLetter(project.name).toLowerCase()
-        configStr.append("public static final String  NAME = \"$NAME\";\n").append("}")
-
-        val filePath = "auto.$groupName.${TextUtils.numOrLetter(project.name).toLowerCase()}.${TextUtils.className(project.name)}Config".replace(".", "/") + ".java"
-        FileUtils.writeText(File(javaCacheDir, filePath), configStr.toString(), true)
-
-        val enterFile = File(javaCacheDir, "auto/com/pqixing/configs/Enter.java")
-        if (buildAsApp) {
-            val enterStr = StringBuilder("package auto.com.pqixing.configs;\n public class Enter { \n")
-                    .append("public static final String  LAUNCH = \"${CONFIG}Launch\";\n")
-                    .append("public static final String  CONFIG = \"${CONFIG}Config\";\n")
-                    .append("}")
-            FileUtils.writeText(enterFile, enterStr.toString(), true)
-        } else FileUtils.delete(enterFile)
-
-
-    }
+//    private fun compatOldPlugin(dpsExt: DpsExtends) {
+////
+//        val javaCacheDir = File(cacheDir, "java")
+//        val groupName = ManagerPlugin.getExtends().groupName
+//        val configStr = StringBuilder("package auto.$groupName.${TextUtils.numOrLetter(project.name).toLowerCase()};\n")
+//                .append("public class ${TextUtils.className(project.name)}Config { \n")
+//        //Config文件输出
+//        val DP_CONFIGS_NAMES = dpsExt.compiles.map { "auto.$groupName.${TextUtils.numOrLetter(it.moduleName).toLowerCase()}.${TextUtils.className(it.moduleName)}Config" }
+//                .sortedBy { it }.toString()
+//        configStr.append("public static final String  DP_CONFIGS_NAMES = \"${DP_CONFIGS_NAMES.replace("[", "").replace("]", "")}\";\n")
+//        val CONFIG = "auto.$groupName.${TextUtils.numOrLetter(project.name).toLowerCase()}.${TextUtils.className(project.name)}"
+//        configStr.append("public static final String  LAUNCH_CONFIG = \"${CONFIG}Launch\";\n")
+//        val NAME = TextUtils.numOrLetter(project.name).toLowerCase()
+//        configStr.append("public static final String  NAME = \"$NAME\";\n").append("}")
+//
+//        val filePath = "auto.$groupName.${TextUtils.numOrLetter(project.name).toLowerCase()}.${TextUtils.className(project.name)}Config".replace(".", "/") + ".java"
+//        FileUtils.writeText(File(javaCacheDir, filePath), configStr.toString(), true)
+//
+//        val enterFile = File(javaCacheDir, "auto/com/pqixing/configs/Enter.java")
+//        if (buildAsApp) {
+//            val enterStr = StringBuilder("package auto.com.pqixing.configs;\n public class Enter { \n")
+//                    .append("public static final String  LAUNCH = \"${CONFIG}Launch\";\n")
+//                    .append("public static final String  CONFIG = \"${CONFIG}Config\";\n")
+//                    .append("}")
+//            FileUtils.writeText(enterFile, enterStr.toString(), true)
+//        } else FileUtils.delete(enterFile)
+//
+//
+//    }
 
     /**
      * 检查插件类型
--- Q-gradle/src/main/java/com/pqixing/modularization/android/CompatDps.kt
+++ Q-gradle/src/main/java/com/pqixing/modularization/android/CompatDps.kt
@@ -1,5 +1,6 @@
 package com.pqixing.modularization.android
 
+import com.pqixing.Tools
 import com.pqixing.modularization.android.dps.DpsExtends
 import com.pqixing.modularization.base.BaseExtension
 import groovy.lang.Closure
@@ -16,16 +17,18 @@ class CompatDps(project: Project, val dpsExtends: DpsExtends) : BaseExtension(pr
      */
     fun dependModules(closure: Closure<Any?>) {
         dpsExtends.configure(closure)
+        Tools.println("--------------moduleConfig { dependModules{} } is deprecated , use innerDps{ } instead!!!")
     }
 
     /**
      * 兼容旧的设置，但是其实已经无用了
      */
     fun androidConfig(closure: Closure<out Any?>) {
+        Tools.println("--------------androidConfig{} is deprecated and do nothing,you can remove it")
     }
 
     fun mavenTypes(closure: Closure<out Any?>) {
-
+        Tools.println("--------------mavenTypes{} is deprecated and do nothing,you can remove it")
     }
 
     fun toMavenVersion(version:String){
