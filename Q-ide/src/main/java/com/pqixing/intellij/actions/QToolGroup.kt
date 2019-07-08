061c93335323e9565ea6275d7d93bc469a761b6d
pqixing <pqixing>
2019-07-08 21:37:07 +0800

    修复jekin插件异常

--- Q-ide/src/main/java/com/pqixing/intellij/actions/QToolGroup.kt
+++ Q-ide/src/main/java/com/pqixing/intellij/actions/QToolGroup.kt
@@ -10,23 +10,33 @@ import java.io.File
 
 class QToolGroup : DefaultActionGroup() {
 
-    override fun update(e: AnActionEvent) {
-        e.presentation.isEnabledAndVisible = true
-        val project = e.project ?: return
-        val basePath = project.basePath ?: return
-        val projectXmlFile = File(basePath, "templet/project.xml")
-        if (projectXmlFile.exists()) projectConfigs[project] = XmlParser().parseText(projectXmlFile.readText()).get("@baseUrl").toString()
+    override fun update(e: AnActionEvent?) {
+        e?.presentation?.isEnabledAndVisible = true
+
+        initSettingEnable(e?.project)
     }
 
 
     companion object {
         var projectConfigs = mutableMapOf<Project, String?>()
+        var projectInit = mutableSetOf<Project>()
         fun isModulariztionProject(project: Project?): Boolean {
-            return project != null && projectConfigs[project] != null
+            project ?: return false
+            if (!projectInit.contains(project)) initSettingEnable(project)
+            return projectConfigs[project] != null
         }
 
         fun isDachenProject(project: Project?): Boolean {
+            project ?: return false
+            if (!projectInit.contains(project)) initSettingEnable(project)
             return isModulariztionProject(project) && projectConfigs[project] == "http://192.168.3.200/android"
+        }
+
+        private fun initSettingEnable(project: Project?) {
+            val basePath = project?.basePath ?: return
+            projectInit.add(project)
+            val projectXmlFile = File(basePath, "templet/project.xml")
+            if (projectXmlFile.exists()) projectConfigs[project] = XmlParser().parseText(projectXmlFile.readText()).get("@baseUrl").toString()
         }
     }
 
