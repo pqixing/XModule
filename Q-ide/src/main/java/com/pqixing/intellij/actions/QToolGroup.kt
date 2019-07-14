ac170a8a8ba7aa344f7aba7cbb835c8b3e1b628c
pqixing <pqixing>
2019-07-14 16:56:33 +0800

    更新对于大辰工程的判断逻辑

--- Q-ide/src/main/java/com/pqixing/intellij/actions/QToolGroup.kt
+++ Q-ide/src/main/java/com/pqixing/intellij/actions/QToolGroup.kt
@@ -1,42 +1,34 @@
 package com.pqixing.intellij.actions
 
-import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.project.Project
-import com.pqixing.model.ProjectXmlModel
-import groovy.lang.GroovyClassLoader
-import groovy.util.XmlParser
 import java.io.File
 
 class QToolGroup : DefaultActionGroup() {
 
-    override fun update(e: AnActionEvent?) {
-        e?.presentation?.isEnabledAndVisible = true
-
-        initSettingEnable(e?.project)
-    }
-
-
     companion object {
-        var projectConfigs = mutableMapOf<Project, String?>()
-        var projectInit = mutableSetOf<Project>()
+        var configs = mutableMapOf<Project, Pair<Long, Boolean?>?>()
+
         fun isModulariztionProject(project: Project?): Boolean {
             project ?: return false
-            if (!projectInit.contains(project)) initSettingEnable(project)
-            return projectConfigs[project] != null
+            initSettingEnable(project)
+            return configs[project] != null
         }
 
         fun isDachenProject(project: Project?): Boolean {
             project ?: return false
-            if (!projectInit.contains(project)) initSettingEnable(project)
-            return isModulariztionProject(project) && projectConfigs[project] == "http://192.168.3.200/android"
+            initSettingEnable(project)
+            return configs[project]?.second == true
         }
 
         private fun initSettingEnable(project: Project?) {
             val basePath = project?.basePath ?: return
-            projectInit.add(project)
             val projectXmlFile = File(basePath, "templet/project.xml")
-            if (projectXmlFile.exists()) projectConfigs[project] = XmlParser().parseText(projectXmlFile.readText()).get("@baseUrl").toString()
+            if (!projectXmlFile.exists()) {
+                configs[project] = null
+            } else if (projectXmlFile.lastModified() > configs[project]?.first ?: 0L) {//距离上次修改有更新时
+                configs[project] = Pair(projectXmlFile.lastModified(), projectXmlFile.readText().contains("DachenBase"))
+            }
         }
     }
 
